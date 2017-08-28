fbef206e8d2931eb9fc792f8a34666861bc1e977
njubatyrov <nursultanmktl@gmail.com>
2017-08-28 16:29:08 +0200

    Implemented adding sharing node

--- MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/ui/SharingNodeActivity.java
+++ MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/ui/SharingNodeActivity.java
@@ -1,12 +1,25 @@
 package ch.ethz.coss.nervousnet.hub.ui;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ch.ethz.coss.nervousnet.hub.R;
 import ch.ethz.coss.nervousnet.hub.ui.adapters.NervousnetNode;
@@ -14,27 +27,33 @@ import ch.ethz.coss.nervousnet.hub.ui.adapters.NodesArrayAdapter;
 
 public class SharingNodeActivity extends BaseActivity {
 
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+    private EditText nameEditText;
+    private EditText ipEditText;
+    private NodesArrayAdapter adapter;
+    private ListView list;
+
+    private NervousnetNode[] nodesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sharing_node);
 
-        ListView list = (ListView) findViewById(R.id.lst_Nodes);
+        list = (ListView) findViewById(R.id.lst_Nodes);
 
-        final NervousnetNode[] nodesList = getNodeList();
-        NodesArrayAdapter adapter = new NodesArrayAdapter(this, R.layout.node_list_item, nodesList);
-        list.setAdapter(adapter);
+        refreshData();
 
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 vibrate();
-                Toast.makeText(SharingNodeActivity.this, "Do you want to remove " + nodesList[position].nodeName + "?", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SharingNodeActivity.this, "Do you want to remove " + nodesList[position].getName() + "?", Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
 
     }
+
 
     private void vibrate() {
         Vibrator v = (Vibrator) SharingNodeActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
@@ -42,19 +61,87 @@ public class SharingNodeActivity extends BaseActivity {
     }
 
     public void clickedAdd(View v) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("New sharing node");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_dialog_add_new_server, null);
+        builder.setView(dialogView);
+
+        nameEditText = (EditText) dialogView.findViewById(R.id.nameEditText);
+        ipEditText = (EditText) dialogView.findViewById(R.id.IPEditText);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                setSharedPreferences(nameEditText.getText().toString(), ipEditText.getText().toString());
+                refreshData();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create();
+        builder.show();
         vibrate();
         Toast.makeText(SharingNodeActivity.this, "Opens splash to add a new node", Toast.LENGTH_SHORT).show();
     }
 
+    private void refreshData() {
+        nodesList = getNodeList();
+        adapter = new NodesArrayAdapter(this, R.layout.node_list_item, nodesList);
+        list.setAdapter(adapter);
+    }
+
+    private String getSharedPreferences() {
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString("sharingNodesJSON", "[]");
+        return restoredText;
+    }
+
+    private void setSharedPreferences(String name, String IP) {
+        try {
+            JSONObject object = new JSONObject();
+            object.put("name", name);
+            object.put("ip", IP);
+
+            SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+            String sharedNodes = prefs.getString("sharingNodesJSON", "[]");
+            JSONArray jsonArray = new JSONArray(sharedNodes);
+
+            jsonArray.put(object);
+            SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+            editor.putString("sharingNodesJSON", jsonArray.toString());
+            editor.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private NervousnetNode[] getNodeList() {
-        //// TODO: 02/08/2016 get real node names
-        NervousnetNode[] data = new NervousnetNode[]{
-                new NervousnetNode("ETH Main Node"),
-                new NervousnetNode("Alice's Node"),
-                new NervousnetNode("Switzerland"),
-                new NervousnetNode("nervousnet internal"),
+        NervousnetNode[] nodesList = new NervousnetNode[] {
+                new NervousnetNode("LOPLOP", "asdf"),
         };
 
-        return data;
+        try {
+            JSONArray jsonArray = new JSONArray(getSharedPreferences());
+            nodesList = new NervousnetNode[jsonArray.length()];
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+                NervousnetNode node = new NervousnetNode(object.getString("name"), object.getString("ip"));
+                nodesList[i] = node;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+        return nodesList;
     }
 }
